8c275c2b2accb6f098a0875c612c0ab50e2c7375
menahem_erlich <erlichmenahem@gmail.com>
2025-11-12 17:23:00 +0200

    uppercased

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import json
+
 from fastapi import FastAPI
-from string_ops import reverse_str
+from string_ops import reverse_str, to_upper
 
 app = FastAPI()
 
 @app.get("/reverse")
 def reverse_app(text: str):
-    result = reverse_str(text)
+    result = json.dumps(reverse_str(text))
     return result
 
+@app.get("/uppercase/{text}")
+def uppercase(text):
+    result = json.dumps(to_upper(text))
+    return result
